1839c2fe731d01d7f994f4fea0f68cb85c822683
SMuyesser <smuyesser@gmail.com>
2017-05-25 22:04:41 -0700

    registration complete

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/userschema')
+
 // Register
 router.get('/register', function(req, res){
 	res.render('register');
@@ -9,6 +11,47 @@ router.get('/register', function(req, res){
 // Login
 router.get('/login', function(req, res){
 	res.render('login');
+});
+
+// Register User
+router.post('/register', function(req, res) {
+	const name = req.body.name;
+	const email = req.body.email;
+	const username = req.body.username;
+	const password = req.body.password;
+	const password2 = req.body.password2;
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Name is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+	const errors = req.validationErrors();
+
+	if(errors){
+		res.render('register', {
+			errors: errors
+		});
+	} else {
+		const newUser = new User({
+			name: name,
+			email: email,
+			username: username,
+			password: password
+		})
+
+		User.createUser(newUser, function(err, user){
+			if(err) throw err;
+			console.log(user);
+		});
+
+		req.flash('success_msg', 'You are registered and can now login');
+
+		res.redirect('/users/login');
+	}
 });
 
 module.exports = router;
